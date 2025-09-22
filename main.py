879b0740ce6355b4f1299a600d73db2c2ad0d313
porteryen yen <porteryen@gmail.com>
2025-09-22 15:58:12 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
 def fibonacci(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    fibs = [0, 1]
+    for i in range(2, n + 1):
+        fibs.append(fibs[i - 1] + fibs[i - 2])
+    return fibs[n]
 
-for i in range(30):
+for i in range(40):
     print(f"F({i:2}) = {fibonacci(i):7}")
-
